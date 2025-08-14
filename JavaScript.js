5db6395e1b5a16a51d7e081e2df2a2f6f216ad7f
Alexander Davies <208832878+alexanderdavies-dev@users.noreply.github.com>
2025-08-14 19:33:36 +0100

    finishing project

--- JavaScript.js
+++ JavaScript.js
@@ -17,5 +17,62 @@ function getComputerChoice() {
         }
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter Rock, Paper or Scissors: ");
+    return humanChoice;
+}
 
-console.log(getComputerChoice());
+function playGame() {
+    console.log("Starting a five-round game!")
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}, Computer chose ${computerChoice}`);
+
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+        return "draw";
+    }
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        return "human";
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+        return "computer";
+    }
+}
+
+for (let round = 1; round <=5; round++) {
+    console.log(`\n--- Round ${round} ---`);
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+
+    console.log(`Score after round ${round}: Human ${humanScore} - ${computerScore} Computer`);
+}
+
+if (humanScore > computerScore) {
+    console.log("Congrats! You won the game!");
+}
+else if (computerScore> humanScore) {
+    console.log("Bad luck! Computer won the game!");
+}
+else {
+    console.log("The game was a draw!");
+}
+}
+
+playGame()
